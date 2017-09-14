ddad3264cf7fc7f8f0dc917243b62475c67d4d57
tianlman <tianlman@163.com>
2017-09-14 10:29:14 +0800

    商品分类

--- src/views/management/goodsCategory.vue
+++ src/views/management/goodsCategory.vue
@@ -7,28 +7,117 @@
       </el-col>
       <el-col :span="4">
         <div class="grid-content bg-purple-dark">
-          <el-button type="primary" @click="dialogFormVisible = true">新增</el-button>
+          <el-button type="primary" @click="goods_cate">新增</el-button>
         </div>
       </el-col>
     </el-row>
-    <el-row class="" style="margin-top: 10px">
+    <el-row class="goods_cate">
       <el-col :span="24">
-        <div class="grid-content bg-purple-dark">
+        <div class="grid-content bg-purple-dark"  style="margin-top: 20px">
+          <el-table
+            :data="tableData5"
+            style="width: 100%;">
+            <el-table-column type="expand">
+              <template scope="scope">
+                <!--二级分类-->
+                <el-table
+                  class="subTable"
+                  :show-header="1>2"
+                  :data="scope.row.sub_data"
+                  style="width: 100%"
+                  :default-sort = "{prop: 'date', order: 'descending'}"
+                >
+                  <el-table-column  type="expand" sortable  >
+                  </el-table-column>
+                  <el-table-column  prop="name" label="分类名称" >
+                    <template scope="scope">
+                      <span>{{ scope.row.name }}</span>
+                    </template>
+                  </el-table-column>
+                  <el-table-column  prop="id" label="排序">
+                    <template scope="scope">
+                      <span> {{ scope.row.sub_id + '-'+ (1+scope.$index) }}</span>
+                    </template>
+                  </el-table-column>
+                  <el-table-column prop="caozuo" label="操作">
+                    <template scope="scope">
+                      <i class="el-icon-edit"></i>
+                      <i class="el-icon-delete"></i>
+                    </template>
+                  </el-table-column>
+                </el-table>
+              </template>
+            </el-table-column>
+            <el-table-column label="分类名称" prop="id">
+              <template scope="scope">
+                <el-icon name="name"></el-icon>
+                <span >{{ scope.row.name }}</span>
+              </template>
+            </el-table-column>
+            <el-table-column label="排序" prop="id">
+              <template scope="scope">
+                <span >{{ scope.$index+1 }}</span>
+              </template>
+            </el-table-column>
+            <el-table-column label="操作" prop="desc">
+              <template scope="scope">
+                <!--<span >{{ scope.row.shop }}</span>-->
+              <i class="el-icon-edit"></i>
+              <i class="el-icon-delete"></i>
+              </template>
+            </el-table-column>
+          </el-table>
         </div>
       </el-col>
     </el-row>
+    <category-dialog ref="categ"></category-dialog>
   </div>
 </template>
 
 <script lang="">
+  import categoryDialog from './mg_dialog/categoryDialog'
   export default {
     name: '',
     data() {
-      return {}
+      return {
+        tableData5: [{
+          id: '1',
+          name: '棉类',
+          shop: '王小虎夫妻店',
+          shopId: '10333',
+          sub_data:[{
+            sub_id:'1',
+            name:'全棉',
+          },{
+            sub_id:'1',
+            name:'高品质棉',
+          },{
+            sub_id:'1',
+            name:'竹节棉',
+          }]
+        }, {
+          id: '2',
+          name: '麻类',
+          shop: '王小虎夫妻店',
+          shopId: '10333',
+          sub_data:[{
+            sub_id:'2',
+            name:'全棉1',
+          }]
+        }],
+        sub_data:[{
+            id:'1',
+            name:'全棉',
+        }]
+      }
     },
-    components: {},
+    components: {
+      categoryDialog,
+    },
     methods: {
-
+      goods_cate(){
+          this.$refs.categ.category()
+      }
     },
     mounted() {
 
@@ -41,5 +130,18 @@
 
 <style lang="scss">
   @import 'src/style/index.scss';
+ .el-table__expanded-cell{
+   padding: 0;
+ }
+  .subTable{
+    .el-icon-arrow-right,.el-table__expanded-cell{
+      display: none;
+    }
+  }
+  .goods_cate .el-icon-edit,.goods_cate .el-icon-delete{
+    display: inline-block;
+    margin: 0 3%;
+    color:#1d90e6;
+  }
 </style>
 
---
+++ src/views/management/mg_dialog/categoryDialog.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="cate_dia">
+    <el-dialog title="添加商品" :visible.sync="dialogFormVisible" size="siny">
+      <el-form :label-position="labelPosition" label-width="80px" :model="formLabelAlign">
+        <el-form-item label="分类名称">
+          <el-input v-model="formLabelAlign.name"></el-input>
+        </el-form-item>
+        <el-form-item label="上级分类">
+          <el-select v-model="value7" placeholder="顶级分类">
+            <el-option-group
+              v-for="group in options3"
+              :key="group.label"
+              :label="group.label">
+              <el-option
+                v-for="item in group.options"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value">
+              </el-option>
+            </el-option-group>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="排序">
+          <el-input v-model="formLabelAlign.type"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <div class="btn">
+            <el-button type="primary" @click="">保 存</el-button>
+            <el-button @click="dialogFormVisible=false">取 消</el-button>
+          </div>
+        </el-form-item>
+      </el-form>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+  export default {
+    data(){
+      return {
+        labelPosition: 'right',
+        dialogFormVisible:false,
+        formLabelAlign: {
+          name: '',
+          region: '',
+          type: ''
+        },
+        options3: [{
+          label: '热门城市',
+          options: [{
+            value: 'Shanghai',
+            label: '上海'
+          }, {
+            value: 'Beijing',
+            label: '北京'
+          }]
+        }, {
+          label: '城市名',
+          options: [{
+            value: 'Chengdu',
+            label: '成都'
+          }, {
+            value: 'Shenzhen',
+            label: '深圳'
+          }, {
+            value: 'Guangzhou',
+            label: '广州'
+          }, {
+            value: 'Dalian',
+            label: '大连'
+          }]
+        }],
+        value7: ''
+      }
+    },
+    components: {},
+    computed: {},
+    methods: {
+      category(){
+        this.dialogFormVisible = true;
+      }
+    },
+  }
+</script>
+
+<style lang="scss">
+  .cate_dia{
+    .el-dialog {
+      width: 400px;
+      .el-input{
+        width:230px;
+      }
+    }
+  .btn{
+    text-align: center;
+    margin-left: -80px;
+  }
+  }
+
+</style>
